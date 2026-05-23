e319fa224398b8577b861e92bb7ac25e3d7cd257
zakyaSharmeen <zakyasharmeen@gmail.com>
2026-05-24 01:56:53 +0530

    final-set-up

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5000",
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_SERVER_URL || "http://localhost:5000",
 
   headers: { "Content-Type": "application/json" },
 });
